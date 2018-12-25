95c9100b79cff4852b1cc916a3b31e3c1e8b7e00
lenonlin <liang.lin@tinklabs.com>
2018-12-25 14:13:25 +0800

    [feature]fix 404 page not error message

--- SSO-Frontend/pages/index.js
+++ SSO-Frontend/pages/index.js
@@ -5,6 +5,7 @@ import './global.less';
 import styles from './styles.less';
 import Button from './components/Button';
 import Breakline from './components/Breakline';
+import Alert from "./registerQ";
 
 
 
@@ -12,6 +13,8 @@ import Breakline from './components/Breakline';
 class IndexPage extends Component {
     static getInitialProps({query}) {
         const clientId = query.appid;
+        const ssoError = query.ssoError;
+        const jwt = query.jwt;
 
         const fbLoginUri = process.env.SERVERURI + `/v1/thirdParty/facebookLogin`
             + `?appid=`+clientId
@@ -22,11 +25,94 @@ class IndexPage extends Component {
         + `&redirect_uri=${encodeURIComponent(process.env.URL)}`
         ;
 
-        return {clientId, fbLoginUri,googleLoginUri};
+        return {clientId, fbLoginUri,googleLoginUri,ssoError,jwt};
     }
 
+
+
     render() {
-        const {t, clientId} = this.props;
+        const {t} = this.props;
+
+        // if (this.props.ssoError) {
+        //     window.history.clear()
+        // }
+
+
+        if(this.props.ssoError){
+            //用户在fb授权页面时，点击了取消按钮。需要清除history信息
+            if(this.props.ssoError === 'The code can\'t be empty, please fill the code'){
+
+                return (
+                    <Container>
+                        <Content>
+                            <div className={styles.logo}>
+                                <img src="/static/logo.png"/>
+                            </div>
+                            <div className={styles.slogan}>
+                                <Trans i18nKey="Become a hi member">
+                                    Become a <i>hi</i> member
+                                </Trans>
+                                <div className={styles.caption}>
+                                    {t(`and enjoy our premium features`)}
+                                </div>
+                            </div>
+                            <div className={styles.buttons}>
+                                <Button className={styles.facebook} href={this.props.fbLoginUri}>
+                                    {t(`Sign in with Facebook`)}
+                                </Button>
+                                <Button className={styles.google} href={this.props.googleLoginUri}>
+                                    {t(`Sign in with Google+`)}
+                                </Button>
+                                <Breakline>{t(`or`)}</Breakline>
+                                <Button className={styles.email} href={`/register?appid=`+this.props.clientId}>
+                                    {t(`Register using Email`)}
+                                </Button>
+                            </div>
+                        </Content>
+                        <Footer>
+                            <div className={styles.signIn}>
+                                {t(`Already have an account?`)}
+                                <a href={`/authorize?appid=`+this.props.clientId}>{t(`SIGN IN`)}</a>
+                            </div>
+                        </Footer>
+                    </Container>
+                );
+            }else {
+                return (
+                    <Container>
+                        <Content>
+                            <div className={styles.slogan}>
+                                <div className={styles.caption}>
+                                    {t(this.props.ssoError)}
+                                </div>
+                            </div>
+                        </Content>
+                        <Footer>
+                            <Button type="button" href={`/index?appid=`+ this.props.clientId}>{t(`login again`)}</Button>
+                        </Footer>
+
+
+                    </Container>
+                );
+            }
+
+
+        }
+
+        if(this.props.jwt){
+            return (
+                <Container>
+                    <Content>
+                        <div className={styles.slogan}>
+                            <div className={styles.caption}>
+                                 {t(`loding......`)}
+                            </div>
+                        </div>
+                    </Content>
+                </Container>
+            );
+        }
+
         return (
             <Container>
                 <Content>
