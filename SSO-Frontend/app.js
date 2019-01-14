b868aa08bc67bfede33ba7205ccae2f6c93d40d6
ScoutYe <jianfei.ye@tinklabs.com>
2019-01-14 10:51:57 +0800

    update

--- SSO-Frontend/app.js
+++ SSO-Frontend/app.js
@@ -1,56 +1,56 @@
-import path from 'path';
-import next from 'next';
-import express from 'express';
-import bodyParser from 'body-parser';
-import i18nextMiddleware from 'i18next-express-middleware';
-import Backend from 'i18next-node-fs-backend';
-import errorHandler from './middleware/errorHandler';
-import createOAuthRoutes from './routes/oauth';
-import createMemberRoutes from './routes/member';
-import createPagesRoutes from './routes/pages';
-import i18nConfig from './i18n.config';
-import i18n from './i18n';
+import path from 'path'
+import next from 'next'
+import express from 'express'
+import bodyParser from 'body-parser'
+import i18nextMiddleware from 'i18next-express-middleware'
+import Backend from 'i18next-node-fs-backend'
+import errorHandler from './middleware/errorHandler'
+import createOAuthRoutes from './routes/oauth'
+import createMemberRoutes from './routes/member'
+import createPagesRoutes from './routes/pages'
+import i18nConfig from './i18n.config'
+import i18n from './i18n'
 
-
-const port = process.env.PORT || 4000;
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({dev});
-const handle = app.getRequestHandler();
-
+const port = process.env.PORT || 4000
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
 
 i18n
-    .use(Backend)
-    .use(i18nextMiddleware.LanguageDetector)
-    .init({
-        ...i18nConfig,
-        backend: {
-            loadPath: path.join(__dirname, '/locales/{{lng}}/{{ns}}.json'),
-        },
-    }, () => {
-        app.prepare()
-            .then(() => {
-                const server = express();
+  .use(Backend)
+  .use(i18nextMiddleware.LanguageDetector)
+  .init(
+    {
+      ...i18nConfig,
+      backend: {
+        loadPath: path.join(__dirname, '/locales/{{lng}}/{{ns}}.json')
+      }
+    },
+    () => {
+      app.prepare().then(() => {
+        const server = express()
 
-                server.use(bodyParser.urlencoded({extended: true}));
-                server.use(bodyParser.json());
-                server.use(i18nextMiddleware.handle(i18n));
-                server.use('/locales', express.static(path.join(__dirname, '/locales')));
+        server.use(bodyParser.urlencoded({ extended: true }))
+        server.use(bodyParser.json())
+        server.use(i18nextMiddleware.handle(i18n))
+        server.use('/locales', express.static(path.join(__dirname, '/locales')))
 
-                // API
-                // server.use('/oauth', createOAuthRoutes(app, server));
-                // server.use('/member', createMemberRoutes(app, server));
+        // API
+        // server.use('/oauth', createOAuthRoutes(app, server));
+        // server.use('/member', createMemberRoutes(app, server));
 
-                // handle API error
-                // server.use(errorHandler);
+        // handle API error
+        // server.use(errorHandler);
 
-                // aws health check
-                server.use('/health', (req, res) => res.send('success'));
+        // aws health check
+        server.use('/health', (req, res) => res.send('success'))
 
-                // pages
-                server.use('/', createPagesRoutes(app, server));
-                server.get('*', (req, res) => handle(req, res));
+        // pages
+        server.use('/', createPagesRoutes(app, server))
+        server.get('*', (req, res) => handle(req, res))
 
-                server.listen(port);
-                console.log('Server is starting!');
-            });
-    });
+        server.listen(port)
+        console.log('Server is starting!')
+      })
+    }
+  )
